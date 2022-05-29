6b27454123941f76c83bfd23af04a628b76ff6d5
dell <tasneim.hatem@gmail.com>
2022-05-29 18:26:16 +0200

    add base fragment

---
+++ app/src/main/java/com/example/e_commerceapp/ui/me/MeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerceapp.ui.me
+
+import android.os.Bundle
+import android.view.View
+import com.example.e_commerceapp.base.ui.BaseFragment
+import com.example.e_commerceapp.databinding.FragmentMeBinding
+
+class MeFragment : BaseFragment<FragmentMeBinding>(FragmentMeBinding::inflate){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
