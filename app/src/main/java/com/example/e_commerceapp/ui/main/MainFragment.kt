65f85ca8842b4eaaba922d56551fd4708aa539bc
dell <tasneim.hatem@gmail.com>
2022-05-30 10:20:58 +0200

    add navigation

--- app/src/main/java/com/example/e_commerceapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/main/MainFragment.kt
@@ -1,8 +1,30 @@
 package com.example.e_commerceapp.ui.main
 
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.e_commerceapp.R
 import com.example.e_commerceapp.base.ui.BaseFragment
 import com.example.e_commerceapp.databinding.FragmentMainBinding
 
 class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate){
+    lateinit var localNavController: NavController
+
+    override fun afterOnViewCreated() {
+        super.afterOnViewCreated()
+        setUpBottomNavigation()
+        setOnDestinationChangedListener()
+    }
+
+    private fun setUpBottomNavigation() {
+        localNavController =
+            (childFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment).navController
+        binding.bottomNavigationView.setupWithNavController(localNavController)
+    }
+
+
+    open fun setOnDestinationChangedListener(){
+
+    }
 
 }
