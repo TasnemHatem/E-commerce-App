5a117c59da5cffa328d641794c5188adce17cc3a
abdelwahabzayed <ammz1997@gmail.com>
2022-05-30 14:15:34 +0200

    upgrade gradle version

--- app/src/main/java/com/example/e_commerceapp/ui/me/MeFragment.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/me/MeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.View
 import com.example.e_commerceapp.base.ui.BaseFragment
 import com.example.e_commerceapp.databinding.FragmentMeBinding
+import com.example.e_commerceapp.local.AppSharedPreference
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MeFragment : BaseFragment<FragmentMeBinding>(FragmentMeBinding::inflate){
-
+    @Inject lateinit var appSharedPreference: AppSharedPreference
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     }
 }
