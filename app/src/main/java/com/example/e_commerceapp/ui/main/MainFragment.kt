cff975872a8b9dfd487f1962767a1181ba7b40ea
abdelwahabzayed <ammz1997@gmail.com>
2022-05-30 05:50:08 +0200

    graphs added

---
+++ app/src/main/java/com/example/e_commerceapp/ui/main/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.example.e_commerceapp.ui.main
+
+import com.example.e_commerceapp.base.ui.BaseFragment
+import com.example.e_commerceapp.databinding.FragmentMainBinding
+
+class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate){
+
+}
