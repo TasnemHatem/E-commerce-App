0e8a64d53af8e0f29fd141f43c691eee41d00235
abdelwahabzayed <ammz1997@gmail.com>
2022-05-30 09:44:18 +0200

    bottom navigation

---
+++ app/src/main/java/com/example/e_commerceapp/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package com.example.e_commerceapp.ui.favorite
+
+import com.example.e_commerceapp.base.ui.BaseFragment
+import com.example.e_commerceapp.databinding.FragmentFavouriteBinding
+
+class FavoriteFragment : BaseFragment<FragmentFavouriteBinding>(FragmentFavouriteBinding::inflate) {
+}
