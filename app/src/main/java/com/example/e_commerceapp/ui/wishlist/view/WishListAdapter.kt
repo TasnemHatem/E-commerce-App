115f23e5d8f7c89a6a01e62db9df8bfa77dd4926
Toba <mohamedtowba298@gmail.com>
2022-06-01 22:14:33 +0200

    Revert "merge"
    This reverts commit b9c79beacf41d49c328ba70ce51078aeccb8e529.

--- app/src/main/java/com/example/e_commerceapp/ui/wishlist/view/WishListAdapter.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/wishlist/view/WishListAdapter.kt
@@ -4,32 +4,28 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.example.e_commerceapp.R
-import com.example.e_commerceapp.databinding.FragmentMeWhishlistRowBinding
 import com.example.e_commerceapp.ui.wishlist.model.DraftOrder
 import com.example.e_commerceapp.ui.wishlist.model.WishlistResponse
 
 
 class WishListAdapter (var context: Context, var data: List<DraftOrder>) : RecyclerView.Adapter<WishListAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        //val binding = ItemListBinding.bind(view)
 
-        val binding = FragmentMeWhishlistRowBinding.bind(itemView)
 
-//        lateinit var img: ImageView
-//        lateinit var price: TextView
-//        lateinit var deleteIcon: ImageView
-//
+//        lateinit var dayDay: TextView
+//        lateinit var dayIcon: ImageView
+//        lateinit var dayDesc: TextView
+//        lateinit var dayTemp: TextView
+//        lateinit var layout: View
 //
 //        init {
-//            //layout = itemView
-////            img = binding.wishlistImgId
-////            price = binding.wishlistPriceId
-////            deleteIcon = binding.deleteIconId
+//            layout = itemView
+//            dayDay = layout.findViewById(R.id.dailyDayNameId)
+//            dayIcon = layout.findViewById(R.id.dailyIconId)
+//            dayDesc = layout.findViewById(R.id.dailyDescId)
+//            dayTemp = layout.findViewById(R.id.dailyTempId)
 //        }
     }
 
@@ -42,8 +38,7 @@ class WishListAdapter (var context: Context, var data: List<DraftOrder>) : Recyc
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.wishlistPriceId.text = data[0].lineItems[position].price
-        holder.binding.wishlistImgId.load(data[0].lineItems[position].title)
+
     }
 
     override fun getItemCount(): Int {
