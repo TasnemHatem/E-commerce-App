2e3ca37609fdd39bbc76225005206970e0b9cbb6
Toba <mohamedtowba298@gmail.com>
2022-06-01 22:30:25 +0200

    fixing merge issues

--- app/src/main/java/com/example/e_commerceapp/di/RepoModule.kt
+++ app/src/main/java/com/example/e_commerceapp/di/RepoModule.kt
@@ -4,9 +4,14 @@ import com.example.e_commerceapp.local.AppSharedPreference
 import com.example.e_commerceapp.ui.auth.network.AuthService
 import com.example.e_commerceapp.ui.auth.repo.AuthRepo
 import com.example.e_commerceapp.ui.auth.repo.AuthRepoImpl
+import com.example.e_commerceapp.ui.category.network.CategoryService
+import com.example.e_commerceapp.ui.category.repo.CategoryRepo
+import com.example.e_commerceapp.ui.category.repo.CategoryRepoImpl
 import com.example.e_commerceapp.ui.home.network.VendorService
 import com.example.e_commerceapp.ui.home.repo.VendorsRepo
 import com.example.e_commerceapp.ui.home.repo.VendorsRepoImpl
+import com.example.e_commerceapp.ui.wishlist.network.WishlistService
+import com.example.e_commerceapp.ui.wishlist.repo.WishlistRepo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,5 +32,17 @@ object RepoModule {
     @Provides
     fun getAuthRepo(authService: AuthService,appSharedPreference: AppSharedPreference): AuthRepo {
         return AuthRepoImpl(authService,appSharedPreference)
+    }
+
+    @ViewModelScoped
+    @Provides
+    fun getCategoryRepo(categoryService: CategoryService): CategoryRepo {
+        return CategoryRepoImpl(categoryService)
+    }
+
+    @ViewModelScoped
+    @Provides
+    fun getWishlistRepo(wishlistService: WishlistService): WishlistRepo {
+        return WishlistRepo(wishlistService)
     }
 }
--- app/src/main/java/com/example/e_commerceapp/di/ServicesModule.kt
+++ app/src/main/java/com/example/e_commerceapp/di/ServicesModule.kt
@@ -2,7 +2,9 @@ package com.example.e_commerceapp.di
 
 import com.example.e_commerceapp.BuildConfig.SERVER_URL
 import com.example.e_commerceapp.ui.auth.network.AuthService
+import com.example.e_commerceapp.ui.category.network.CategoryService
 import com.example.e_commerceapp.ui.home.network.VendorService
+import com.example.e_commerceapp.ui.wishlist.network.WishlistService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,6 +30,17 @@ object ServicesModule {
         return getDynamicRetrofitClient(client).create(AuthService::class.java)
     }
 
+    @ViewModelScoped
+    @Provides
+    fun providesCategoryService(client: OkHttpClient): CategoryService {
+        return getDynamicRetrofitClient(client).create(CategoryService::class.java)
+    }
+
+    @ViewModelScoped
+    @Provides
+    fun providesWishlistService(client: OkHttpClient): WishlistService {
+        return getDynamicRetrofitClient(client).create(WishlistService::class.java)
+    }
 
     private fun getDynamicRetrofitClient(
         client: OkHttpClient,
