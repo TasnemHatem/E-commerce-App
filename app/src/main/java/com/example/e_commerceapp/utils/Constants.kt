dead8dbdbcc6f8c1464f82e6c40c61d92b710aba
Toba <mohamedtowba298@gmail.com>
2022-06-08 13:59:08 +0200

    adding payment screen

--- app/src/main/java/com/example/e_commerceapp/utils/Constants.kt
+++ app/src/main/java/com/example/e_commerceapp/utils/Constants.kt
@@ -6,4 +6,5 @@ object Constants {
     const val SHARED_CART_ID = "shared_cart_id"
     const val SHARED_FAV_ID = "shared_favourite_id"
     const val SHARED_NAME = "shared_name_id"
+    const val PAYPAL_KEY="ATR2jife_ThjLkOd7Fb5ADeFa0LbvKj1GSQsLBYeB5alrswGaNrfZh_7Noob8oBDGR_PY36UJqPbJSxm"
 }
