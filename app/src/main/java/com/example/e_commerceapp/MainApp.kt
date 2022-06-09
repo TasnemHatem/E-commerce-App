504ad356b64dd031d1a6ab00245d58eb6e69ac27
Toba <mohamedtowba298@gmail.com>
2022-06-09 15:09:24 +0200

    modify payment screen

--- app/src/main/java/com/example/e_commerceapp/MainApp.kt
+++ app/src/main/java/com/example/e_commerceapp/MainApp.kt
@@ -5,4 +5,20 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApp: Application() {
+    /*
+    override fun onCreate() {
+        super.onCreate()
+        val config = CheckoutConfig(
+            application = this,
+            clientId = YOUR_CLIENT_ID,
+            environment = Environment.SANDBOX,
+            returnUrl = "BuildConfig.APPLICATION_ID":
+            currencyCode = CurrencyCode.USD,
+        userAction = UserAction.PAY_NOW,
+        settingsConfig = SettingsConfig(
+            loggingEnabled = true
+        )
+        )
+        PayPalCheckout.setConfig(config)
+    }*/
 }
