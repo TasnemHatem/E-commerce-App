d74e5268f31c960910e43f71ea36a329566151f5
EmanAbobakr <43084491+EmanAbobakr@users.noreply.github.com>
2022-06-10 13:28:36 +0200

    finishing currency logic

--- app/src/main/java/com/example/e_commerceapp/utils/Constants.kt
+++ app/src/main/java/com/example/e_commerceapp/utils/Constants.kt
@@ -8,4 +8,6 @@ object Constants {
     const val SHARED_NAME = "shared_name_id"
     const val PAYPAL_KEY="ATR2jife_ThjLkOd7Fb5ADeFa0LbvKj1GSQsLBYeB5alrswGaNrfZh_7Noob8oBDGR_PY36UJqPbJSxm"
     const val CURRENCY_SERVER_URL="https://v6.exchangerate-api.com/"
+    const val SHARED_CURRENCY_CODE = "shared_currency_code"
+    const val SHARED_CURRENCY_VALUE = "shared_currency_value"
 }
