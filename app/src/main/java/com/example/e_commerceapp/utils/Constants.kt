5a10f596d2675ab880b9892dff764b49bcb958cc
abdelwahabzayed <ammz1997@gmail.com>
2022-06-18 17:59:59 +0200

    check login before go to cart

--- app/src/main/java/com/example/e_commerceapp/utils/Constants.kt
+++ app/src/main/java/com/example/e_commerceapp/utils/Constants.kt
@@ -6,6 +6,7 @@ object Constants {
     const val SHARED_CART_ID = "shared_cart_id"
     const val SHARED_FAV_ID = "shared_favourite_id"
     const val SHARED_NAME = "shared_name_id"
+    const val SHARED_LOGIN_KEY = "login"
     const val PAYPAL_KEY="AeBFdQd0r362tTATsKqMFzH1duQw50RqbpgVot026AXkv5m1xsgxMPdJHGjO06u9Pi2sgCQpq3qtgysz"
     const val CURRENCY_SERVER_URL="https://v6.exchangerate-api.com/"
     const val SHARED_CURRENCY_CODE = "shared_currency_code"
